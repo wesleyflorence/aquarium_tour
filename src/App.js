931dcfef44b4fbee2bb1c3d1ef6f90656e7a42d1
Wes Florence <hello@wesflorence.com>
2020-03-20 16:29:29 +0000

    adding some icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo_white.png';
 import './App.css';
 
 function App() {
@@ -7,19 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> HELLO WES HERE </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Virtual Tour </p>
       </header>
+    <p> Life on the Edge </p>
+    <p> Puget Sound Hall </p>
+    <p> Pier 59 </p>
+    <p> Pier 60 </p>
+    <p> Mezzanine Level </p>
+    <p> Boeing Lobby </p>
     </div>
   );
 }
