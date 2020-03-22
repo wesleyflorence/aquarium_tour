2bc9f40abddc78055ddf3718fbd29084defc20d8
Wes Florence <hello@wesflorence.com>
2020-03-22 19:34:52 +0000

    working on modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import './Splash.css';
+import './Popup.css';
 
-import { Splash } from './Splash';
+// import { Splash } from './Splash';
 import { Tour } from './Tour';
 
 function App() {
   return (
     <div className="App">
-      <Splash />
       <Tour />
     </div>
   );
--- src/Tour.js
+++ src/Tour.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import map from './Map.png';
+import map from './assets/Map.png';
 import './Tour.css';
+import PopupVideo from './Popup';
 
 class Tour extends React.Component {
   render() {
     return (
-    <div className='Tour'>
-    Life on the Edge
-    Puget Sound Hall
-    Pier 59
-    Pier 60
-    Mezzanine Level
-    Boeing Lobby
-        <img src={map} className='Tour-Map' alt="aquarium map" />
-    </div>
+      <div className='Tour'>
+      <PopupVideo />
+      <ul className='Tour-List'>
+      <li key="lifeOnTheEdge"><a href='https://google.com'> Life on the Edge</a></li>
+      <li key="pugetSoundHall">Puget Sound Hall</li>
+      <li key="pier59">Pier 59</li>
+      <li key="pier60">Pier 60</li>
+      <li key="mezzanineLevel">Mezzanine Level</li>
+      <li key="boeingLobby">Boeing Lobby</li>
+      </ul>
+      <img src={map} className='Tour-Map' alt="aquarium map" />
+      </div>
     );
   }
 }
