94360829f0e33ed56e798068f50aa92fad646814
Wes Florence <hello@wesflorence.com>
2020-03-22 23:41:30 +0000

    check in

--- src/Tour.js
+++ src/Tour.js
@@ -2,8 +2,14 @@ import React from 'react';
 import Popup from "reactjs-popup";
 import aquarium_map from './assets/Map.png';
 import pier59Pin from './assets/pins/pier59-pin.png';
+import pier60Pin from './assets/pins/pier60-pin.png';
+import lifePin from './assets/pins/life-pin.png';
+import pugetPin from './assets/pins/puget-pin.png';
+import mezzPin from './assets/pins/mezz-pin.png';
+import boeingPin from './assets/pins/boeing-pin.png';
+import galleryPin from './assets/pins/gallery-pin.png';
+import pacificPin from './assets/pins/pacific-pin.png';
 import './Tour.css';
-
 
 class Tour extends React.Component {
   constructor() {
@@ -41,8 +47,9 @@ class Tour extends React.Component {
 
       <div className='Tour-Map'>
       <img src={aquarium_map} className='Tour-Map-Image' alt="aquarium map"/>
-      <button onClick={() => this.onClick("https://www.youtube.com/embed/cHjbQsWWOew")}><img src={pier59Pin} className='pin' alt="pin"/></button>
-      <div/>
+      <button className='pier59' onClick={() => this.onClick("https://www.youtube.com/embed/cHjbQsWWOew")}><img src={pier59Pin} className='pin' alt="pin"/></button>
+      <button className='pier60' onClick={() => this.onClick("https://www.youtube.com/embed/87CVAoNPbLM")}><img src={pier60Pin} className='pin' alt="pin"/></button>
+      </div>
 
       <ul className='Tour-List'>
       <li key="lifeOnTheEdge"><button onClick={() => this.onClick("https://www.youtube.com/embed/-FeNdH0ylq4")}>Life on the Edge</button></li>
@@ -54,7 +61,6 @@ class Tour extends React.Component {
       <li key="transitionalgallery"><button onClick={() => this.onClick("https://www.youtube.com/embed/4MSkt3FlYCc")}>Transitional Gallery</button></li>
       <li key="pacificcoralreef"><button onClick={() => this.onClick("https://www.youtube.com/embed/sDbuEj2P2ZM")}>Pacific Coral Reef</button></li>
       </ul>
-      </div>
       </div>
     );
   }
