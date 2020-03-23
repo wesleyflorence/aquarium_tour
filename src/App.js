c4aed372f5f596ac63b03a78f4d6b941be7dd5b1
Wes Florence <hello@wesflorence.com>
2020-03-23 01:29:02 +0000

    splash screen

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from 'react';
 import './App.css';
 import './Splash.css';
 import './Popup.css';
+import ScrollableAnchor, { goToAnchor, configureAnchors } from "react-scrollable-anchor";
 
-// import { Splash } from './Splash';
+import { Splash } from './Splash';
 import { Tour } from './Tour';
 
-function App() {
-  return (
-    <div className="App">
-      <Tour />
-    </div>
-  );
+configureAnchors({scrollDuration: 2000, keepLastAnchorHash: false})
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <a href="#tour">
+        <Splash />
+      </a>
+        <ScrollableAnchor id={"tour"}>
+          <Tour />
+        </ScrollableAnchor>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Splash.js
+++ src/Splash.js
@@ -8,6 +8,7 @@ class Splash extends React.Component {
       <div className="Splash">
       <header className="Splash-header">
       <img src={logo} className="Splash-logo" alt="logo" />
+      <p> Virtual Tour </p>
       </header>
       </div>
     );
